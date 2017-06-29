c97fa70928a73abe6f649e861b616600aa0be49f
Pierre Burton <pburton@alliance-bokiau.com>
2017-06-29 08:22:38 +0200

    Back-office front-end in progress

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+/* Middlewares */
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
+
+/* Routes */
 app.get('/', function (req, res) {
 	res.render('dashboard', { title: 'Hey', message: 'Hello there!', link: 'Go to test', link2: 'test'})
 });
@@ -12,6 +16,9 @@ app.get('/test', function (req, res) {
 	res.render('dashboard', { title: 'Oh', message: 'This is the test page', link: 'Go to root', link2: '/' })
 });
 
+
+
+/* 3, 2, 1, Launch ! */
 app.listen(3000, function() {
 	console.log('App is up and running on 3000!');
 })
